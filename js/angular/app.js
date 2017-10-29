67bf25b4d787b9205ed3809b5078163caa06aa2e
Rodrigo Medina <rodrigo.medinacetz@gmail.com>
2017-10-28 22:35:37 -0500

    Estructura del proyecto

--- js/angular/app.js
+++ js/angular/app.js
@@ -2,4 +2,14 @@ var app = angular.module('bookingApp', ['ngRoute', 'ui.bootstrap']);
 
 app.controller('mainCtrl', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
 
+    /**
+     * Contact Form
+     */
+    $scope.formInput = {};
+
+    $scope.validarCorreo = function () {
+        console.log('Validar formulario');
+        console.log($scope.contactForm.$valid);
+        console.log($scope.formInput);
+    };
 }]);
